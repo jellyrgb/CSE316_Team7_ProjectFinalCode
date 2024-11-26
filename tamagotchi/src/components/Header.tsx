9389c73fb137411c3f6b97a13a0788cd51fcd931
Hyomin <hyomin.kim@stonybrook.edu>
2024-11-26 20:07:54 +0900

    update header and navbar

---
+++ tamagotchi/src/components/Header.tsx
@@ -0,0 +1,11 @@
+import '../css/Header.css';
+
+function Header() {
+  return (
+    <header className="header-container">
+      <h1 className="header-title">Home Page</h1>
+    </header>
+  );
+}
+
+export default Header;
